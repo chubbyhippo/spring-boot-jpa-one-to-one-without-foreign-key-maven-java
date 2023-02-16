6b318289c482be47768f90e723b1250c4cc10e48
ch <mkmsacc@outlook.com>
2023-02-16 21:59:26 +0700

    Add toString to book entity

--- src/main/java/com/example/demo/entity/Book.java
+++ src/main/java/com/example/demo/entity/Book.java
@@ -1,10 +1,7 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
@@ -12,6 +9,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "book")
+@ToString
 public class Book {
 
     @Id
