ffce9b7f8216809e8fae1530a87555b16fb610e5
ch <mkmsacc@outlook.com>
2023-02-16 21:59:50 +0700

    Add test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private StudentRepository repository;
+
+    @Test
+    void shouldGetStudentBooks() {
+        repository.findAll().forEach(student -> System.out.println(student.getBook()));
+    }
 
 }
