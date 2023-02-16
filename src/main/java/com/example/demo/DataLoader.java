fda8b4443822ed609f97ffb2b056553820ff8373
ch <mkmsacc@outlook.com>
2023-02-16 20:31:34 +0700

    Add data loader

---
+++ src/main/java/com/example/demo/DataLoader.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import com.example.demo.entity.Book;
+import com.example.demo.entity.Student;
+import com.example.demo.repository.BookRepository;
+import com.example.demo.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader {
+
+    private final BookRepository bookRepository;
+    private final StudentRepository studentRepository;
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            bookRepository.save(new Book(1L, "Hippo", 9.99, "Hippo Study"));
+            studentRepository.save(new Student(1L, "Hippo", 3.99));
+        };
+    }
+}
