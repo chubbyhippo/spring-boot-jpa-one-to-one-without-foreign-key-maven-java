2d656684de1a7d2f664247561367735e5a8f108a
ch <mkmsacc@outlook.com>
2023-02-17 08:12:55 +0700

    Format schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 CREATE TABLE BOOK
 (
     id     BIGINT PRIMARY KEY NOT NULL,
-    author VARCHAR(255)        NOT NULL,
-    price  DOUBLE               NOT NULL,
-    title  VARCHAR(255)        NOT NULL
+    author VARCHAR(255)       NOT NULL,
+    price  DOUBLE             NOT NULL,
+    title  VARCHAR(255)       NOT NULL
 );
 
 CREATE TABLE STUDENT
 (
     id   BIGINT PRIMARY KEY NOT NULL,
-    name VARCHAR(255)    NOT NULL,
-    gpa  DOUBLE         NOT NULL
+    name VARCHAR(255)       NOT NULL,
+    gpa  DOUBLE             NOT NULL
 );
 
