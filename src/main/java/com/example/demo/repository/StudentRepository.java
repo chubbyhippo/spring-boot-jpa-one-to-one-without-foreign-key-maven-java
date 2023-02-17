82145a471c4157a95cfb56307bbe856b8f927a73
ch <mkmsacc@outlook.com>
2023-02-17 08:57:42 +0700

    Rename repo method

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.dto.StudentBookDtoInterface;
+import com.example.demo.dto.StudentBookDto;
 import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,5 +10,5 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query(value = "select Student.name, Book.title from Student inner join Book on Student.name = Book.author",
             nativeQuery = true)
-    List<StudentBookDtoInterface> findStudentBooksDtoInterface();
+    List<StudentBookDto> findStudentBooks();
 }
