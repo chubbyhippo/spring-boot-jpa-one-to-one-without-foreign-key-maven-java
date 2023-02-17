cb6446ed186bdb09787d330a976126c7e309375b
ch <mkmsacc@outlook.com>
2023-02-17 08:19:55 +0700

    Add find student book

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo.repository;
 
+import com.example.demo.dto.StudentBookDtoInterface;
 import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value = "select Student.name, Book.title from Student inner join Book on Student.name = Book.author",
+            nativeQuery = true)
+    List<StudentBookDtoInterface> findStudentBooks();
 }
