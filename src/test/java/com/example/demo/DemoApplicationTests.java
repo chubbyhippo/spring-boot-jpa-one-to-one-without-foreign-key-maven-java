18eed09ed9c21b3747ad220687a269e079798453
ch <mkmsacc@outlook.com>
2023-02-17 07:47:20 +0700

    Refactor test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import com.example.demo.repository.StudentRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -13,7 +16,14 @@ class DemoApplicationTests {
 
     @Test
     void shouldGetStudentBooks() {
-        repository.findAll().forEach(student -> System.out.println(student.getBook()));
+        var result = repository.findAll().stream().map(student -> {
+
+            System.out.println(student.getBook());
+
+            return student.getBook();
+        }).toList();
+
+        assertThat(result).isNotEmpty();
     }
 
 }
