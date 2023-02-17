e7b5e89bc5b7eaebf091d405c664167cf541c3a3
ch <mkmsacc@outlook.com>
2023-02-17 08:20:02 +0700

    Add find student book test

---
+++ src/test/java/com/example/demo/repository/StudentRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.repository;
+
+import com.example.demo.config.DataLoader;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import(DataLoader.class)
+class StudentRepositoryTest {
+
+    @Test
+    void shouldGetStudentBooks(@Autowired StudentRepository repository) {
+        repository.findStudentBooks().forEach(studentBookDto -> {
+
+            assertThat(studentBookDto.getName()).isNotNull();
+            System.out.println(studentBookDto.getName());
+
+            assertThat(studentBookDto.getTitle()).isNotNull();
+            System.out.println(studentBookDto.getTitle());
+
+        });
+    }
+}
