c26dde9300f0981c21d2d6e7b33ce582a061db23
ch <mkmsacc@outlook.com>
2023-02-17 07:50:03 +0700

    Move autowire repo to the single test param

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,21 +1,17 @@
 package com.example.demo;
 
 import com.example.demo.repository.StudentRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Autowired
-    private StudentRepository repository;
-
     @Test
-    void shouldGetStudentBooks() {
+    void shouldGetStudentBooks(@Autowired StudentRepository repository) {
         var result = repository.findAll().stream().map(student -> {
 
             System.out.println(student.getBook());
